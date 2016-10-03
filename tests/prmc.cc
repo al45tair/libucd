aa6b73d902c20f3350626d36ece0370198a883fa
Alastair Houghton <alastair@coriolis-systems.com>
2016-10-03 14:41:06 +0100

    Fixed warnings.

--- tests/prmc.cc
+++ tests/prmc.cc
@@ -11,26 +11,26 @@ TEST_CASE("we can find Primary Composites from character pairs", "[prmc]") {
   codepoint cp;
 
   cp = db.primary_composite('e', 0x0301);
-  REQUIRE(cp == 0xe9);
+  REQUIRE(cp == 0xe9u);
 
   cp = db.primary_composite('n', 0x0303);
-  REQUIRE(cp == 0xf1);
+  REQUIRE(cp == 0xf1u);
 
   cp = db.primary_composite('!', 0x030a);
-  REQUIRE(cp == 0);
+  REQUIRE(cp == 0u);
 
   cp = db.primary_composite(0x1100, 0x1161);
-  REQUIRE(cp == 0xac00);
+  REQUIRE(cp == 0xac00u);
 
   cp = db.primary_composite(0x1107, 0x116a);
-  REQUIRE(cp == 0xbd10);
+  REQUIRE(cp == 0xbd10u);
 
   cp = db.primary_composite(0xbd10, 0x11a9);
-  REQUIRE(cp == 0xbd12);
+  REQUIRE(cp == 0xbd12u);
 
   cp = db.primary_composite(0xbd10, 0x303);
-  REQUIRE(cp == 0);
+  REQUIRE(cp == 0u);
 
   cp = db.primary_composite('e', 'e');
-  REQUIRE(cp == 0);
+  REQUIRE(cp == 0u);
 }
